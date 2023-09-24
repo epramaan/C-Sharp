21a89d4bf42d3b3296fd52c64f3a76e64264b5df
epramaan <pratikdhote@cdac.in>
2023-09-24 11:58:17 +0530

    masked sesitive data

--- OtvOidcDotNetProduction/OidcDotNetProduction/Controllers/EpramaanController.cs
+++ OtvOidcDotNetProduction/OidcDotNetProduction/Controllers/EpramaanController.cs
@@ -23,9 +23,9 @@ namespace OIDC_DOT_NET_INTEGRATION_PRODUCTION.Controllers
         public static readonly string token_request_uri = "https://epramaan.meripehchaan.gov.in/openid/jwt/processJwtTokenRequest.do";
         public static readonly string push_back_uri = "https://epramaan.meripehchaan.gov.in/rest/epramaan/enrol/response";
 
-        public static readonly string client_id = "100001231";
-        public static readonly string salt = "100001";
-        public static readonly string aeskey = "cb3a59f6-0617-4898-b859-8bb02fee91b3";
+        public static readonly string client_id = "1*********1231";
+        public static readonly string salt = "1****1";
+        public static readonly string aeskey = "c*********************b3";
         public static readonly string redirect_uri = "http://localhost:44355/Epramaan/ProcessAuthCodeAndGetToken";
         public static readonly string Certificate = "D:/Integration/aspDotNet/OidcDotNetProduction/OidcDotNetProduction/epramaanprod2016.cer";
 
